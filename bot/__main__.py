1683d120cfc0bfed15bffc402beeb25b6af0fbce
SStandby <standbyy9@gmail.com>
2022-11-21 16:20:25 -0500

    add: incorporation of filters and commands

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python
 # pylint: disable=unused-argument, wrong-import-position
 # This program is dedicated to the public domain under the CC0 license.
+
 import logging
 import telebot
 from bot import kaino
 from bot.common import importdir
+from bot.common.util.binance_check import BinanceClient, IsBinance
 from telebot.async_telebot import asyncio_filters
+
 # Enable logging
+logging.getLogger('prisma').setLevel(logging.DEBUG)
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
     )
 logger = logging.getLogger(__name__)
 
+
 importdir.do('bot/commands/auth', globals())
+importdir.do('bot/commands/finance', globals())
+
 
 start_text = """
 ╔══════════════════════╗
@@ -29,6 +36,8 @@ async def start(message):
 
 kaino.add_custom_filter(asyncio_filters.StateFilter(kaino))
 kaino.add_custom_filter(asyncio_filters.IsDigitFilter())
+kaino.add_custom_filter(IsBinance())
+kaino.setup_middleware(BinanceClient())
 
 if __name__ == "__main__":
     import asyncio
