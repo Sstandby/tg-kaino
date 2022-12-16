ec9cc031a2780b0a3e25a342b123fa182b762328
SStandby <standbyy9@gmail.com>
2022-12-15 22:23:48 -0500

    refactor: filters

--- bot/common/filters/user_filter.py
+++ bot/common/filters/user_filter.py
@@ -3,8 +3,8 @@ from telebot.asyncio_filters import SimpleCustomFilter
 from bot.common.db.users import existing_user
 from binance.exceptions import BinanceAPIException
 
-class IsBinance(SimpleCustomFilter):
-    key='binance_user'
+class IsUserDB(SimpleCustomFilter):
+    key='existing_user'
     @staticmethod
     async def check(message: telebot.types.Message):
         if await existing_user(message.from_user.username): return True
