95c7192a4421ddfe122e5b58ed07b1e9ea2397a4
SStandby <standbyy9@gmail.com>
2022-12-31 20:37:41 -0500

    refactor: style code

--- bot/__main__.py
+++ bot/__main__.py
@@ -8,7 +8,7 @@ from bot.common import importdir
 from telebot.async_telebot import asyncio_filters
 from bot.common.filters.admin_filter import IsAdmin
 from bot.common.filters.user_filter import IsUserDB, IsMembership
-from bot.common.handlers.user_middleware import UserDBMiddlware
+from bot.common.handlers.user_middleware import UserDBMiddlware, StateDeleteMiddlware
 from bot.common.handlers.binance_middleware import BinanceClientMiddleware
 from bot.common.handlers.antiflood_middleware import AntiFloodMiddleware
 # Enable logging
@@ -23,21 +23,8 @@ importdir.do('bot/commands/auth', globals())
 importdir.do('bot/commands/finance/futures', globals())
 importdir.do('bot/commands/finance/coinpayments', globals())
 importdir.do('bot/commands/admin', globals())
+importdir.do('bot/commands/info', globals())
 importdir.do('bot/common/exceptions', globals())
-
-
-start_text = """
-╔══════════════════════╗
-    ¡✩ Aqui kaino presentandose ✩!
-   ❝ Recuerde seleccionar la opción
-            que mas se ajusta a sus
-                     necesidades. ❞
-╚══════════════════════╝
-"""
-
-@kaino.message_handler(commands=['start'])
-async def start(message):
-    await kaino.reply_to(message, start_text)
 
 kaino.add_custom_filter(asyncio_filters.StateFilter(kaino))
 kaino.add_custom_filter(asyncio_filters.IsDigitFilter())
@@ -46,6 +33,7 @@ kaino.add_custom_filter(IsUserDB())
 kaino.add_custom_filter(IsMembership())
 kaino.setup_middleware(BinanceClientMiddleware())
 kaino.setup_middleware(UserDBMiddlware())
+kaino.setup_middleware(StateDeleteMiddlware())
 kaino.setup_middleware(AntiFloodMiddleware(3))
 
 if __name__ == "__main__":
